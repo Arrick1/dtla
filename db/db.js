4a6f38e2d8555bb8e54ec1d8f2d3930c6d301a84
Arrick <Grapes01@gmail.com>
2019-06-11 16:29:58 -0700

    fix

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dtlagi', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dtla', {
   useNewUrlParser: true,
   UseCreateIndex: true,
   useFindAndModify: true
