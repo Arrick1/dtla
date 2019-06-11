158c3630a30d1a039984cd3a98df441e519cb26d
Arrick <Grapes01@gmail.com>
2019-06-11 14:40:04 -0700

    set up

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const mongoose = require()
+require('./db/db')
+
+
+app.get('/', (req, res) => {
+    res.send('hello')
+}) 
+
+app.listen(3000, () => {
+    console.log('port:', 3000)
+})
