1ebbcc23fc388c6834f895811fb2df93455e60e5
Jomar Mendoza <jmm21@Jomars-MBP.home>
2019-06-11 23:24:33 -0700

    the AddService can take the input of the user and change the state

--- src/Components/AddService/AddService.js
+++ src/Components/AddService/AddService.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 class AddService extends Component {
+  state = {
+    name:"",
+  	city:"",
+  	state:"",
+  	zip:"",
+  	phone:"",
+  	email:"",
+  	web:"",
+  	day:[],
+  	hours:[],
+  	eligibility:"",
+  	description:""
+  }
+
+  inputHandler = (e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }
+
   render(){
+    const { name, city, state, zip, phone, email, web, day, hours, eligibility, description } = this.state
     return(
       <div>
-        add service
+        <form>
+          <input type='text' placeholder='name' name='name' value={name} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='city' name='city' value={city} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='state' name='state' value={state} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='zip' name='zip' value={zip} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='phone' name='phone' value={phone} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='email' name='email' value={email} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='web' name='web' value={web} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='day' name='day' value={day} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='hours' name='hours' value={hours} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='eligibility' name='eligibility' value={eligibility} onChange={this.inputHandler}></input>
+          <input type='text' placeholder='description' name='description' value={description} onChange={this.inputHandler}></input>
+        </form>
       </div>
     )
   }
